6e31e0aac0143b5f688e4a632233b6654f2228f8
Moises Soto <musoto96@gmail.com>
2024-01-18 21:02:39 -0600

    Added xsession

--- link.sh
+++ link.sh
@@ -6,9 +6,13 @@ unlink ~/.tmux.conf
 unlink ~/.kshrc
 unlink ~/.private_aliases
 unlink ~/.fvwm
+unlink ~/.xsession
+unlink ~/.xinitrc
 ln -s $SCRIPT_DIR/vim ~/.vim
 ln -s $SCRIPT_DIR/vim/vimrc ~/.vimrc
 ln -s $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
 ln -s $SCRIPT_DIR/fvwm ~/.fvwm
 ln -s $SCRIPT_DIR/ksh/kshrc ~/.kshrc
 ln -s $SCRIPT_DIR/ksh/private_aliases ~/.private_aliases
+ln -s $SCRIPT_DIR/xsession ~/.xsession
+ln -s $SCRIPT_DIR/xsession ~/.xinitrc
