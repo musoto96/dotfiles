b5922a1b49cf7874b2900b4135a8bbaa4ddf9fad
Moises Soto <musoto96@gmail.com>
2024-01-18 21:05:37 -0600

    Added Xresources

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ unlink ~/.private_aliases
 unlink ~/.fvwm
 unlink ~/.xsession
 unlink ~/.xinitrc
+unlink ~/.Xresources
 ln -s $SCRIPT_DIR/vim ~/.vim
 ln -s $SCRIPT_DIR/vim/vimrc ~/.vimrc
 ln -s $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
@@ -16,3 +17,4 @@ ln -s $SCRIPT_DIR/ksh/kshrc ~/.kshrc
 ln -s $SCRIPT_DIR/ksh/private_aliases ~/.private_aliases
 ln -s $SCRIPT_DIR/xsession ~/.xsession
 ln -s $SCRIPT_DIR/xsession ~/.xinitrc
+ln -s $SCRIPT_DIR/Xresources ~/.Xresources
