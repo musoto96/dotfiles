377c99acb283f1d933d83b270abd7e07b1f4814a
Moises Soto <musoto96@gmail.com>
2024-01-19 22:52:40 -0600

    Added icewm configs

--- link.sh
+++ link.sh
@@ -9,12 +9,16 @@ unlink ~/.fvwm
 unlink ~/.xsession
 unlink ~/.xinitrc
 unlink ~/.Xresources
+unlink ~/.icewm
 ln -s $SCRIPT_DIR/vim ~/.vim
 ln -s $SCRIPT_DIR/vim/vimrc ~/.vimrc
 ln -s $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
 ln -s $SCRIPT_DIR/fvwm ~/.fvwm
+ln -s $SCRIPT_DIR/icewm ~/.icewm
 ln -s $SCRIPT_DIR/ksh/kshrc ~/.kshrc
 ln -s $SCRIPT_DIR/ksh/private_aliases ~/.private_aliases
+ln -s $SCRIPT_DIR/Xorg/Xresources ~/.Xresources
+
+chmod ug+x $SCRIPT_DIR/Xorg/xsession
 ln -s $SCRIPT_DIR/Xorg/xsession ~/.xsession
 ln -s $SCRIPT_DIR/Xorg/xsession ~/.xinitrc
-ln -s $SCRIPT_DIR/Xorg/Xresources ~/.Xresources
