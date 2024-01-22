2b386ba15a2685da5a69b236caa5d7c4bd7ff671
Moises Soto <musoto96@gmail.com>
2024-01-21 22:03:57 -0600

    Added xdm and some sample images

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SCRIPT_DIR=$(dirname $0)
+SCRIPT_DIR=$(cd `dirname $0`;echo $PWD)
 unlink ~/.vim
 unlink ~/.vimrc
 unlink ~/.tmux.conf
@@ -10,6 +10,9 @@ unlink ~/.xsession
 unlink ~/.xinitrc
 unlink ~/.Xresources
 unlink ~/.icewm
+sudo unlink /etc/X11/xdm/Xresources
+sudo unlink /etc/X11/xdm/Xsetup_0
+sudo unlink /usr/include/X11/pixmaps/palm_trees.jpg
 ln -s $SCRIPT_DIR/vim ~/.vim
 ln -s $SCRIPT_DIR/vim/vimrc ~/.vimrc
 ln -s $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
@@ -22,3 +25,11 @@ ln -s $SCRIPT_DIR/Xorg/Xresources ~/.Xresources
 chmod ug+x $SCRIPT_DIR/Xorg/xsession
 ln -s $SCRIPT_DIR/Xorg/xsession ~/.xsession
 ln -s $SCRIPT_DIR/Xorg/xsession ~/.xinitrc
+
+sudo ln -s $SCRIPT_DIR/xdm/Xresources /etc/X11/xdm/Xresources
+sudo ln -s $SCRIPT_DIR/xdm/Xsetup_0 /etc/X11/xdm/Xsetup_0
+sudo chown root:root $SCRIPT_DIR/xdm/Xresources
+sudo chown root:root $SCRIPT_DIR/xdm/Xsetup_0
+
+mkdir -p /usr/include/X11/pixmaps/
+sudo ln -s $SCRIPT_DIR/xdm/pixmaps/palm_trees.jpg /usr/include/X11/pixmaps/palm_trees.jpg
